6411a60ce7f690b4bec3baeecb4063e067c4634f
djcsdj <djcsdj@hotmail.com>
2024-09-06 10:36:52 +0300

    ex5 calc

--- src/week_1/Calculation.java
+++ src/week_1/Calculation.java
@@ -2,15 +2,10 @@ package week_1;
 
 public class Calculation {
     public static void main (String[] args){
-        int first=6;
-        int second=7;
-        System.out.println(first+second);
-        int calcWithParens= 1+1+3*(first+second);
-        System.out.println(calcWithParens);
-        int calcWithoutParens=1+1;
-        calcWithoutParens=calcWithoutParens+3*first;
-        calcWithoutParens=calcWithoutParens+second;
-        System.out.println(calcWithoutParens);
-
+        int days_in_year=365;
+        int hours_in_day=24;
+        int minutes_in_hour=60;
+        int seconds_in_minute=60;
+        System.out.println("There are "+days_in_year*hours_in_day*minutes_in_hour*seconds_in_minute+" seconds in a year.");
     }
 }
