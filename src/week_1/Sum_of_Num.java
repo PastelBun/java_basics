c8220be1eceebaf7f7162806c30d0427ae7843ee
djcsdj <djcsdj@hotmail.com>
2024-09-06 11:14:16 +0300

    ex_26 sum of many numbers

---
+++ src/week_1/Sum_of_Num.java
@@ -0,0 +1,22 @@
+package week_1;
+import java.util.Scanner;
+
+public class Sum_of_Num {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int sum = 0;
+        while (true) {
+            int read = Integer.parseInt(reader.nextLine());
+            if (read == 0) {
+                break;
+            }
+            else {
+                sum = sum + read;
+
+                System.out.println("Sum now: " + sum);
+            }
+        }
+
+        System.out.println("Sum in the end: " + sum);
+    }
+}
