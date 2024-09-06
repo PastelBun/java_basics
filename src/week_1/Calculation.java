be27346715321730434bd2bc1902ef2e3b7f2fec
djcsdj <djcsdj@hotmail.com>
2024-09-06 10:57:17 +0300

    ex_5 calc

---
+++ src/week_1/Calculation.java
@@ -0,0 +1,11 @@
+package week_1;
+
+public class Calculation {
+    public static void main (String[] args){
+        int days_in_year=365;
+        int hours_in_day=24;
+        int minutes_in_hour=60;
+        int seconds_in_minute=60;
+        System.out.println("There are "+days_in_year*hours_in_day*minutes_in_hour*seconds_in_minute+" seconds in a year.");
+    }
+}
