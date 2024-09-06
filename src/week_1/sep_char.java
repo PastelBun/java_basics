e3164c21d491347aee4b86976423c79a59479a06
djcsdj <djcsdj@hotmail.com>
2024-09-06 11:51:51 +0300

    ex_50 seperating first character

---
+++ src/week_1/sep_char.java
@@ -0,0 +1,15 @@
+package week_1;
+import java.util.Scanner;
+
+public class sep_char {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String read = reader.nextLine();
+        int i=0;
+        while (read.length() != i) {
+            System.out.println((i+1)+". character: "+ read.charAt(i));
+            i++;
+        }
+    }
+}
