3382bba8ccaeebe8dd7cc302caf20c43df1911e0
Sam <djcsdj@hotmail.com>
2025-02-26 10:30:05 +0200

    save point week 8, ex 9 WIP

---
+++ src/week_8/exercise_9/VehicleRegister.java
@@ -0,0 +1,16 @@
+package week_8.exercise_9;
+
+public class VehicleRegister {
+    public boolean add(RegistrationPlate plate, String owner){
+        if (plate == null) {
+            return false;
+        }
+        else if (owner != null) {
+            return false;
+        }
+        else {
+            return true;
+        }
+
+    }
+}
